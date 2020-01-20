857c83baed3c1f290592cf6cb51670f149fb95c1
Kathleen Garrity <garrityk@bc.edu>
2020-01-20 15:59:19 -0500

    Initial Commit

---
+++ Two-Button Challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Two-Button Challenge
+//
+//  Created by Kathleen Garrity on 1/20/20.
+//  Copyright © 2020 Kathleen Garrity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
